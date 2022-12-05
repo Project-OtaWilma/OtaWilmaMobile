261c32e93925971bc7b62194c284ba9a3c754595
zexee24 <31950088+zexee24@users.noreply.github.com>
2022-12-05 12:20:22 +0200

    Told some prewritten shit to fuck off

--- app/src/main/java/com/otawilma/mobileclient/data/model/LoggedInUser.kt
+++
@@ -1,9 +0,0 @@
-package com.otawilma.mobileclient.data.model
-
-/**
- * Data class that captures user information for logged in users retrieved from LoginRepository
- */
-data class LoggedInUser(
-    val userId: String,
-    val displayName: String
-)
