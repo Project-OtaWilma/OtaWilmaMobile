eb8b2d00f3e554129ed58ea28eaa2ffe7bf1ba04
zexee24 <31950088+zexee24@users.noreply.github.com>
2023-03-15 17:04:34 +0200

    Fixed changed endpoints

--- app/src/main/java/com/otawilma/mobileclient/Globals.kt
+++ app/src/main/java/com/otawilma/mobileclient/Globals.kt
@@ -5,7 +5,7 @@ import okhttp3.OkHttpClient
 import java.time.Duration
 
 
-const val OTAWILMA_API_URL = "https://beta.wilma-api.tuukk.dev/api"
+const val OTAWILMA_API_URL = "https://wilma.otawilma.fi/api/"
 const val SCHOOLDAY_FILES_DIR_NAME = "schooldays"
 const val MESSAGE_FILES_DIR_NAME = "messages"
 val JUMP_LESSON_THRESHOLD: Duration = Duration.ofMinutes(30)
